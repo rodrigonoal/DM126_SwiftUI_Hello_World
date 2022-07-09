dd742a238da9d651269672653003390469f7aecc
Rodrigo Noal <rodrigonoal@gmail.com>
2022-07-09 15:54:55 -0400

    build: simple modularization

--- SwiftUI_Hello_World/ContentView.swift
+++ SwiftUI_Hello_World/ContentView.swift
@@ -1,25 +1,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    //state variables will automactically update the screen
-    //when their values are updated
-    @State private var text: String = "Hello, Rodrigo"
-    @State private var show = true
+    @State private var product = Product(name: "", description: "", code: "", price: 0.0)
+    
+    @State private var isValidCode = true
+    @State private var isProductSaved = false
+    
+    private func saveProduct(){
+        isValidCode = product.code.count >= 5
+        isProductSaved = isValidCode
+    }
     
     var body: some View {
-        
-        VStack {
-            Text(text)
-                .padding()
-                .foregroundColor(.black)
-            Button("Click me") {
-                text = "Button clicked"
-                show.toggle()
+        Form {
+            Section(header: Text("Product")) {
+                VStack(alignment: .leading){
+                    Text("Name:")
+                    TextField("Enter the name", text: $product.name)
+                }
+                VStack(alignment: .leading) {
+                    Text("Description:")
+                    TextField("Enter the description", text: $product.description)
+                }
+                VStack(alignment: .leading) {
+                    Text("Code:")
+                    HStack {
+                        TextField("Enter the code", text: $product.description)
+                        if (!self.isValidCode) {
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
             }
             
-            if(show) {
-                Text("Teste")
+            Section {
+                Button(action: saveProduct) {
+                    Text("Save")
+                }
             }
+            
+            if(self.isProductSaved) {
+                ProductSavedView(product: self.product)
+            }
+            
+        }
+    }
+}
+
+struct ProductSavedView: View{
+    var product: Product
+    
+    var body: some View {
+        Section {
+            Text("Name: \(product.name)")
+            Text("Description: \(product.description)")
+            Text("Code: \(product.code)")
+            Text("Price: \(product.price)")
         }
     }
 }
@@ -27,6 +64,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.portraitUpsideDown)
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
---
+++ SwiftUI_Hello_World/Product.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Product {
+    var name: String
+    var description: String
+    var code: String
+    var price: Double
+}

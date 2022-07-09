304ac631dcb7999dbccaa885aff41941415f97b6
Rodrigo Noal <rodrigonoal@gmail.com>
2022-07-09 16:08:40 -0400

    build: simple modularization

--- SwiftUI_Hello_World/ContentView.swift
+++ SwiftUI_Hello_World/ContentView.swift
@@ -1,8 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var product = Product(name: "", description: "", code: "", price: 0.0)
-    
+    @State private var product = Product()
     @State private var isValidCode = true
     @State private var isProductSaved = false
     
@@ -13,26 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         Form {
-            Section(header: Text("Product")) {
-                VStack(alignment: .leading){
-                    Text("Name:")
-                    TextField("Enter the name", text: $product.name)
-                }
-                VStack(alignment: .leading) {
-                    Text("Description:")
-                    TextField("Enter the description", text: $product.description)
-                }
-                VStack(alignment: .leading) {
-                    Text("Code:")
-                    HStack {
-                        TextField("Enter the code", text: $product.description)
-                        if (!self.isValidCode) {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .foregroundColor(.red)
-                        }
-                    }
-                }
-            }
+            ProductCreationView(product: $product, isValidCode: isValidCode)
             
             Section {
                 Button(action: saveProduct) {
@@ -40,10 +20,38 @@ struct ContentView: View {
                 }
             }
             
+            
             if(self.isProductSaved) {
                 ProductSavedView(product: self.product)
             }
-            
+        }
+    }
+}
+
+struct ProductCreationView: View {
+    @Binding var product: Product
+    var isValidCode: Bool
+    
+    var body: some View{
+        Section(header: Text("Product")) {
+            VStack(alignment: .leading){
+                Text("Name:")
+                TextField("Enter the name", text: $product.name)
+            }
+            VStack(alignment: .leading) {
+                Text("Description:")
+                TextField("Enter the description", text: $product.description)
+            }
+            VStack(alignment: .leading) {
+                Text("Code:")
+                HStack {
+                    TextField("Enter the code", text: $product.description)
+                    if (!self.isValidCode) {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .foregroundColor(.red)
+                    }
+                }
+            }
         }
     }
 }
--- SwiftUI_Hello_World/Product.swift
+++ SwiftUI_Hello_World/Product.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct Product {
-    var name: String
-    var description: String
-    var code: String
-    var price: Double
+    var name = ""
+    var description = ""
+    var code = ""
+    var price = 0.0
 }

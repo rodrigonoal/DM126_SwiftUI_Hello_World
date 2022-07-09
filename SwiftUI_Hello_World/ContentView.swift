378bc25b35a41308922036687b2e8ede215dadac
Rodrigo Noal <rodrigonoal@gmail.com>
2022-07-09 14:13:40 -0400

    build: initial commit

---
+++ SwiftUI_Hello_World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Hello_World
+//
+//  Created by user219835 on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Rodrigo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Hello_World/SwiftUI_Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Hello_WorldApp.swift
+//  SwiftUI_Hello_World
+//
+//  Created by user219835 on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
